d0f008a478ca64c1e2fa2e2fbd7568f6d3fe65e9
sathishkumar <sathish91@macappstudio.com>
2021-11-19 21:04:36 +0530

    Image, MP3 and MP4 download

---
+++ app/src/main/java/com/project/pdfdownload/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.project.pdfdownload
+
+import android.Manifest
+import android.app.DownloadManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val STORAGE_PERMISSION_CODE :Int = 1000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        downloadBtn.setOnClickListener {
+            if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
+                if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), STORAGE_PERMISSION_CODE)
+
+                }
+                else   {
+                   startDownloading()
+                   }
+
+               }
+                else{
+                startDownloading()
+            }
+        }
+    }
+
+    private fun startDownloading() {
+        val url =urlEt.text.toString()
+        val request = DownloadManager.Request(Uri.parse(url))
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI or DownloadManager.Request.NETWORK_MOBILE)
+        request.setTitle("Download")
+        request.setDescription("The file is downloading...")
+
+        request.allowScanningByMediaScanner()
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"${System.currentTimeMillis()}" )
+
+        val manager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        manager.enqueue(request)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+           STORAGE_PERMISSION_CODE->{
+               if (grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                   startDownloading()
+               }else{
+               Toast.makeText(this,"Permission Denied..", Toast.LENGTH_SHORT).show()
+               }
+           }
+       }
+    }
+}
